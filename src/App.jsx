db46a79f36ea26dad2a6dc1813fc877a9d1e2488
lakshmank6300 <lakshmank6300@gmail.com>
2025-10-25 11:28:27 +0530

    Dashboard developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Typo Speed</h1>
-      <h1>Starting the Web Login</h1>
+      {/* <h1>Typo Speed</h1>
+      <h1>Starting the Web Login</h1> */}
+      <Home/>
+      
+
     </div>
   )
 }
