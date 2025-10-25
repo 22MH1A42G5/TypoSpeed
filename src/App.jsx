087f001223a62ea02a1d8cd70967eb5da7463dfb
ram <kamparapusriram@gmail.com>
2025-10-25 13:22:42 +0530

    updated conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
-import React from "react";
 import {Routes , Route} from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
