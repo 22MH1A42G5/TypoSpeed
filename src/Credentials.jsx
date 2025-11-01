d6c2de79439921d1812500db659d051489068ec6
ram <kamparapusriram@gmail.com>
2025-11-01 16:05:43 +0530

    updated environmental variables

--- src/Credentials.jsx
+++ src/Credentials.jsx
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDHSYXzfOYwI8Xmxq5dG3R71_FRuZ2i9mo",
-  authDomain: "typospeed-ee17a.firebaseapp.com",
-  databaseURL: "https://typospeed-ee17a-default-rtdb.firebaseio.com",
-  projectId: "typospeed-ee17a",
-  storageBucket: "typospeed-ee17a.firebasestorage.app",
-  messagingSenderId: "1027928375265",
-  appId: "1:1027928375265:web:a277d9b1d74b453c6bdca0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
